32bf2921c5b24575de69be5ec51e5d7c08dd7475
Vamsee Lakamsani <Vamsee.Lakamsani@kp.org>
2018-01-04 17:58:09 -0800

    added README.md and async tests.

---
+++ README.md
@@ -0,0 +1,7 @@
+Sample to test [Strong Soap](https://github.com/strongloop/strong-soap)                                                                   
+# How to run
+*  Use Node.js 8.9.1
+* `npm install`
+* `npm run build`
+* `npm run start`
+* In a different terminal `npm run test`
--- __tests__/SOAPTests.js
+++ __tests__/SOAPTests.js
@@ -1,13 +1,47 @@
 
 const soap = require('strong-soap').soap
 
-describe('Strong SOAP  Tests', () => {
+const {promisify} = require('util')
 
-  it('should test Strong SOAP',  () => {
-    const serverURL = 'http://localhost:2347/stockquote?WSDL'
+describe('Strong SOAP  Tests', async () => {
 
+  const serverURL = 'http://localhost:2347/stockquote?WSDL'
+
+  it('should test Strong SOAP Async',  async() => {
+    jest.setTimeout(10000);
+    const createClient = soap.createClient
+    const createClientAsync = promisify(createClient)
+    const client = await createClientAsync(serverURL)
+    const description = client.describe()
+    console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
+
+    const method = client['StockQuote']['StockQuoteSoap']['GetQuote']
+
+    const requestArgs = {symbol: 'IBM'}
+
+    /*
+    method(requestArgs, function(err, result, envelope, soapHeader) {
+      //response envelope
+      console.log('Response Envelope: \n' + envelope);
+      //'result' is the response body
+      console.log('Result: \n' + JSON.stringify(result));
+    });
+    */
+
+
+    const asyncMethod = promisify(method)
+    const [result, envelope, soapHeader] = await asyncMethod(requestArgs)
+    console.log('Response Envelope: \n' + envelope);
+    //'result' is the response body
+    console.log('Result: \n' + JSON.stringify(result));
+
+  })
+
+  it.skip('should test Strong SOAP Callbacks',  () => {
     var options = {};
     soap.createClient(serverURL, options, function(err, client) {
+      const description = client.describe()
+      console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
       var method = client['StockQuote']['StockQuoteSoap']['GetQuote'];
       method(requestArgs, function(err, result, envelope, soapHeader) {
         //response envelope
