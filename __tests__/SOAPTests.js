0569a26da5e2e6be5c6f06eebc60d2a7fbf0e6b7
Vamsee Lakamsani <Vamsee.Lakamsani@kp.org>
2018-01-04 20:45:05 -0800

    fixed port numbers to use sandbox/reproducible test case instead of full internal server.

--- __tests__/SOAPTests.js
+++ __tests__/SOAPTests.js
@@ -3,31 +3,24 @@ const soap = require('strong-soap').soap
 
 const {promisify} = require('util')
 
+/*
+ https://groups.google.com/forum/#!forum/loopbackjs
+  https://gitter.im/strongloop/loopback
+ */
+
 describe('Strong SOAP  Tests', async () => {
 
   const serverURL = 'http://localhost:2347/stockquote?WSDL'
+  const requestArgs = {symbol: 'IBM'}
 
   it('should test Strong SOAP Async',  async() => {
     jest.setTimeout(10000);
     const createClient = soap.createClient
     const createClientAsync = promisify(createClient)
     const client = await createClientAsync(serverURL)
-    const description = client.describe()
-    console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
-
+    //const description = client.describe()
+    //console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
     const method = client['StockQuote']['StockQuoteSoap']['GetQuote']
-
-    const requestArgs = {symbol: 'IBM'}
-
-    /*
-    method(requestArgs, function(err, result, envelope, soapHeader) {
-      //response envelope
-      console.log('Response Envelope: \n' + envelope);
-      //'result' is the response body
-      console.log('Result: \n' + JSON.stringify(result));
-    });
-    */
-
 
     const asyncMethod = promisify(method)
     const [result, envelope, soapHeader] = await asyncMethod(requestArgs)
@@ -37,17 +30,20 @@ describe('Strong SOAP  Tests', async () => {
 
   })
 
-  it.skip('should test Strong SOAP Callbacks',  () => {
+  it('should test Strong SOAP Callbacks',  done => {
     var options = {};
     soap.createClient(serverURL, options, function(err, client) {
       const description = client.describe()
-      console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
+
+      //expect(description.StockQuote).toBeDefined()
+      //console.log(JSON.stringify(description.StockQuote.StockQuoteSoap.GetQuote));
       var method = client['StockQuote']['StockQuoteSoap']['GetQuote'];
       method(requestArgs, function(err, result, envelope, soapHeader) {
         //response envelope
         console.log('Response Envelope: \n' + envelope);
         //'result' is the response body
         console.log('Result: \n' + JSON.stringify(result));
+        done()
       });
     });
   })
